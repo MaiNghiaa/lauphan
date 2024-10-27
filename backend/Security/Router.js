869fb36c7bbe1f21456b65fba6050ee8279e7f35
Trung Nghĩa <109063567+MaiNghiaa@users.noreply.github.com>
2024-10-28 01:00:50 +0700

    update Backend

---
+++ backend/Security/Router.js
@@ -0,0 +1,27 @@
+// router.js
+
+"use strict";
+const multer = require("multer");
+const express = require("express");
+const path = require("path");
+const ProductsController = require("./Controllers/CustomerController");
+// const AdminController = require("./Controller/AdminController");
+
+module.exports = function (app) {
+  const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "assets/"); // Thư mục lưu trữ tệp
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + path.extname(file.originalname)); // Đặt tên tệp
+    },
+  });
+
+  const upload = multer({ storage: storage });
+  // Sử dụng middleware để phục vụ file tĩnh từ thư mục 'assets'
+  app.use("/assets", express.static("assets"));
+
+  //cac route
+
+  app.route("/loginAdmin").post(ProductsController.Login);
+};
